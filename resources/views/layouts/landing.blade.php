fd2c90fd370503971f09c9945c83b97b4b44ce5a
Rafli Maulana Firmansyah <rafli.m.firmansyah@gmail.com>
2026-01-14 21:42:26 +0700

    update landing

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/png">
 
-        <title>{{ config('app.name', 'SocBoost') }} - #1 Social Media Growth Service</title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
