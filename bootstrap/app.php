ab81d18866ad226931a4fb34530a1694c40daff7
Rafli Maulana Firmansyah <rafli.m.firmansyah@gmail.com>
2026-01-14 20:51:26 +0700

    update bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
